cf56a5331260cee35387ca591c97f90dc8c631cb
Ashish Ahuja <ahj.ashish@gmail.com>
2015-11-15 15:16:04 +0530

    Initial commit

---
+++ Easy/src/StdInpOut.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Created by artoves on 15/11/15.
+ */
+public class StdInpOut {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
---
+++ Easy/src/WelcomeToJava.java
@@ -0,0 +1,9 @@
+/**
+ * Created by artoves on 15/11/15.
+ */
+public class WelcomeToJava {
+    public static void main(String[] args){
+        System.out.println("Hello World.");
+        System.out.println("Hello Java.");
+    }
+}

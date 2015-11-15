55b685f6dc1fecc77f6519bd3736eb86d6de11a3
Ashish Ahuja <ahj.ashish@gmail.com>
2015-11-15 17:15:32 +0530

    Added Loops to print series along with test cases

---
+++ Easy/src/Loops.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+/**
+ * Created by artoves on 15/11/15.
+ */
+public class Loops {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        int a, b, n = 0;
+        String[] inputs = new String[3];
+        for (int i = 0; i < testcases; i++) {
+            inputs = br.readLine().split(" ");
+            a = Integer.parseInt(inputs[0]);
+            b = Integer.parseInt(inputs[1]);
+            n = Integer.parseInt(inputs[2]);
+            if (a >= 0 && b >= 0 && a <= 50 && b <= 50) {
+                if (n >= 1 && n <= 15) {
+                    Loops loops = new Loops();
+                    System.out.println(loops.series(a, b, n));
+                }
+            }
+        }
+    }
+
+    public String series(int a, int b, int n) {
+        int nextElem = a;
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            nextElem = a;
+            for (int j = 0; j <= i; j++) {
+                nextElem += Math.pow(2, j) * b;
+            }
+            s.append(nextElem + " ");
+        }
+        return s.toString().trim();
+    }
+}
---
+++ Easy/test/LoopsTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by artoves on 15/11/15.
+ */
+public class LoopsTest {
+    @Test
+    public void testSeries1(){
+        Loops loops = new Loops();
+        assertEquals("2 6 14 30 62 126 254 510 1022 2046",loops.series(0,2,10));
+    }
+    @Test
+    public void testSeries2(){
+        Loops loops = new Loops();
+        assertEquals("8 14 26 50 98",loops.series(5,3,5));
+    }
+}

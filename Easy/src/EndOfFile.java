8b9d72049f35f89c392b70370f6e08bf62626fce
Ashish Ahuja <ahj.ashish@gmail.com>
2016-03-18 02:04:33 +0530

    Added EndOfFile
    Added a EndOfFile java class that reads the standard input and prints
    the line prefixed with the line numbers until the EOF (End of File).

---
+++ Easy/src/EndOfFile.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Created by artoves on 18/03/16.
+ */
+public class EndOfFile {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int i = 1;
+        while (sc.hasNext()){
+            System.out.println(i + " " + sc.nextLine());
+            i++;
+        }
+    }
+}
